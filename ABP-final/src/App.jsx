23030398f856b1a60199f702baf6ddec851fba20
MarielarosaSZ <mariela_suarezz@hotmail.com>
2025-07-11 17:20:28 -0300

    agregado de boton Modo oscuro y agregado de estilo en index.css

--- ABP-final/src/App.jsx
+++ ABP-final/src/App.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useState } from 'react'
+import {useEffect, useState, useRef} from 'react'
 import axios from 'axios'
 import './index.css'
 import Stats from './componentes/Stats.jsx'
@@ -7,11 +7,16 @@ function App() {
   const [search, setSearch] = useState('')
   const [products, setProducts] = useState([])
   const [mostrar, setMostrar] = useState(true)
+  const [oscuro, setOscuro] = useState(false) 
 
-  
+  const containerRef = useRef(null)
+  const toggleDarkMode = () => {setOscuro(!oscuro);
+          containerRef.current.classList.toggle('modo-oscuro')}
+
+        
   useEffect(() => {axios.get("https://dummyjson.com/products?limit=100").then(
                   (res)=> {setProducts(res.data.products)}).catch(err => console.error(err))}, [])
-
+  
 
   const filteredProducts = products.filter((product) =>
     product.title.toLowerCase().includes(search.toLowerCase())
@@ -21,11 +26,12 @@ function App() {
   const precioMin = Math.min(...filteredProducts.map(p => p.price));
   const precioProm = (filteredProducts.reduce((sum, p) => sum + p.price, 0) / totalProducts).toFixed(2);
   return (
-    <>
+    <div ref={containerRef}>
       
-      <h1 className='text-3xl text-blue-600 text-center font-bold' >CATÁLOGO DE PRODUCTOS
+      <h1 className='text-3xl text-blue-600 text-center font-bold' >CATÁLOGO DE PRODUCTOS</h1>
 
-      </h1>
+      <button className="bg-blue-800 text-white p-2 m-2 rounded" onClick={toggleDarkMode}> {!oscuro ? "Oscuro":"Claro"}</button>
+
       <input className="border p-2 m-2" type="text" placeholder='Buscar Productos' value={search} 
               onChange={(e) => setSearch(e.target.value)} />
       
@@ -49,7 +55,7 @@ function App() {
       {mostrar && <Stats  total={totalProducts} max={precioMax} min={precioMin} prom={precioProm} />}
       
        
-    </>
+    </div>
   )
 }
 
